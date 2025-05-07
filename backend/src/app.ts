01848b2256f87172c40295845c4e8595478adf30
adarsh <adarshjithu10@gmail.com>
2025-05-07 15:17:51 +0530

    origin changed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: ["http://localhost:3001","http://localhost:3000",'https://test-adarsh-git-main-shijo-thomas-projects.vercel.app'],
+        origin: [true],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
